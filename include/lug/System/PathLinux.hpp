1b4b734c3fc2e8b0638e20ff12a9b88ffb454b21
cchardeau <corentin.chardeau@epitech.eu>
2016-10-09 15:47:44 +0200

    Implement PathLinux class

--- include/lug/System/PathLinux.hpp
+++ include/lug/System/PathLinux.hpp
@@ -1,19 +1,26 @@
 #pragma once
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+
 #include "Path.hpp"
 
-namespace                           lug
+namespace               lug
 {
-    namespace                       System
-    {
-        class               PathLinux : Path {
-        public:
-            PathLinux() = default;
-            ~PathLinux() = default;
-            std::string     getRoot() const;
-            std::string     getHome() const;
-            std::string     getCwd() const;
-            std::string     getSave() const;
-        };
+namespace               System
+{
+    class               PathLinux : Path {
+    public:
+        PathLinux() = default;
+        ~PathLinux() = default;
+        PathLinux(PathLinux const &) = default;
+        PathLinux &operator=(const PathLinux &) = default;
+        std::string     getRoot() const;
+        std::string     getHome() const;
+        std::string     getCwd() const;
+        std::string     getSave() const;
     };
+};
 };
--- src/lug/System/PathLinux.cpp
+++ src/lug/System/PathLinux.cpp
@@ -1,17 +1,36 @@
-#include "../../../include/lug/System/PathLinux.hpp"
+#include "PathLinux.hpp"
 
-std::string     lug::System::PathLinux::getRoot() const {
-    return std::string("getRoot");
+std::string lug::System::PathLinux::getRoot() const {
+    return std::string("/");
 }
 
-std::string     lug::System::PathLinux::getHome() const {
-    return std::string("getHome");
+std::string lug::System::PathLinux::getHome() const {
+    struct passwd *pw = getpwuid(getuid());
+
+    if (pw == NULL) {
+        return std::string("");
+    }
+    return pw->pw_dir;
 }
 
-std::string     lug::System::PathLinux::getCwd() const {
-    return std::string("getCwd");
+std::string lug::System::PathLinux::getCwd() const {
+    return get_current_dir_name();
 }
 
-std::string     lug::System::PathLinux::getSave() const {
-    return std::string("getSave");
+std::string lug::System::PathLinux::getSave() const {
+    struct passwd *pw = getpwuid(getuid());
+    char *homePath = get_current_dir_name();
+    if (pw != NULL) {
+        homePath = pw->pw_dir;
+    }
+
+    std::string savePath(homePath);
+    savePath += "/.lug";
+
+    struct stat st;
+    if (stat(savePath.c_str(), &st) != 0) {
+        mkdir(savePath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    }
+
+    return savePath;
 }
