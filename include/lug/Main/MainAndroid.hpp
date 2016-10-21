fb13ab8b95a68dda25705f30f613fe67f5fe1f03
Yoann Long <yoann.long@epitech.eu>
2016-10-20 22:32:10 -0400

    fix CXX_FLAG for android
    small fix, was CMAKE_CXX_FLAGS and not ${CMAKE_CXX_FLAGS}
    
    Fix formatting

--- include/lug/Main/MainAndroid.hpp
+++ include/lug/Main/MainAndroid.hpp
@@ -5,28 +5,28 @@
 #include <pthread.h>
 #include <lug/Main/Export.hpp>
 
-extern int main(int argc, const char *argv[]);
+extern int main(int argc, const char* argv[]);
 
 namespace lug {
 namespace Main {
 
 struct lug_android_app {
-    ANativeActivity *activity;
-    ANativeWindow *window;
+    ANativeActivity* activity;
+    ANativeWindow* window;
 
-    ALooper *looper;
-    AInputQueue *inputQueue;
-    AConfiguration *config;
+    ALooper* looper;
+    AInputQueue* inputQueue;
+    AConfiguration* config;
 
-    void *savedState;
+    void* savedState;
     size_t savedStateSize;
 
     pthread_mutex_t mutex;
     pthread_cond_t cond;
     pthread_t thread;
 
-    AInputQueue *pendingInputQueue;
-    ANativeWindow *pendingWindow;
+    AInputQueue* pendingInputQueue;
+    ANativeWindow* pendingWindow;
 };
 
 } // Main
--- src/lug/Main/MainAndroid.cpp
+++ src/lug/Main/MainAndroid.cpp
@@ -3,7 +3,7 @@
 namespace lug {
 namespace Main {
 
-static void shutdown_application(struct lug_android_app *app) {
+static void shutdown_application(struct lug_android_app* app) {
     pthread_mutex_lock(&app->mutex);
     if (app->savedState != nullptr) {
         free(app->savedState);
@@ -13,15 +13,15 @@ static void shutdown_application(struct lug_android_app *app) {
     pthread_mutex_unlock(&app->mutex);
 }
 
-static void *start_application(void *_app) {
-    struct lug_android_app* app = static_cast<struct lug_android_app *>(_app);
+static void* start_application(void* _app) {
+    struct lug_android_app* app = static_cast<struct lug_android_app*>(_app);
     main(0, nullptr);
     shutdown_application(app);
     return nullptr;
 }
 
-static struct lug_android_app *lug_app_create(ANativeActivity *activity, void *savedState, size_t savedStateSize) {
-    struct lug_android_app *app = (struct lug_android_app *) malloc(sizeof(struct lug_android_app));
+static struct lug_android_app* lug_app_create(ANativeActivity* activity, void* savedState, size_t savedStateSize) {
+    struct lug_android_app* app = new lug_android_app;
 
     if (savedState != nullptr) {
         app->savedState = malloc(savedStateSize);
@@ -35,96 +35,96 @@ static struct lug_android_app *lug_app_create(ANativeActivity *activity, void *s
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
     pthread_create(&app->thread, &attr, start_application, app);
-    return (app);
+    return app;
+}
+
+static void onStart(ANativeActivity* activity) {
+
+}
+
+static void onResume(ANativeActivity* activity) {
+
+}
+
+static void* onSaveInstanceState(ANativeActivity* activity, size_t* outSize) {
+    return nullptr;
+}
+
+static void onPause(ANativeActivity* activity) {
+
+}
+
+static void onStop(ANativeActivity* activity) {
+
+}
+
+static void onDestroy(ANativeActivity* activity) {
+
+}
+
+static void onWindowFocusChanged(ANativeActivity* activity, int hasFocus) {
+
+}
+
+static void onNativeWindowCreated(ANativeActivity* activity, ANativeWindow* window) {
+
+}
+
+static void onNativeWindowResized(ANativeActivity* activity, ANativeWindow* window) {
+
+}
+
+static void onNativeWindowRedrawNeeded(ANativeActivity* activity, ANativeWindow* window) {
+
+}
+
+static void onNativeWindowDestroyed(ANativeActivity* activity, ANativeWindow* window) {
+
+}
+
+
+static void onInputQueueCreated(ANativeActivity* activity, AInputQueue* queue) {
+
+}
+
+static void onInputQueueDestroyed(ANativeActivity* activity, AInputQueue* queue) {
+
+}
+
+static void onContentRectChanged(ANativeActivity* activity, const ARect* rect) {
+
+}
+
+static void onConfigurationChanged(ANativeActivity* activity) {
+
+}
+
+static void onLowMemory(ANativeActivity* activity) {
+
 }
 
 } // Main
 } // lug
 
-static void onStart(ANativeActivity *activity) {
+void  ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_t savedStateSize) {
+    activity->callbacks->onInputQueueCreated = lug::Main::onInputQueueCreated;
+    activity->callbacks->onInputQueueDestroyed = lug::Main::onInputQueueDestroyed;
 
-}
+    activity->callbacks->onNativeWindowCreated = lug::Main::onNativeWindowCreated;
+    activity->callbacks->onNativeWindowDestroyed = lug::Main::onNativeWindowDestroyed;
+    activity->callbacks->onNativeWindowResized = lug::Main::onNativeWindowResized;
+    activity->callbacks->onNativeWindowRedrawNeeded = lug::Main::onNativeWindowRedrawNeeded;
+    activity->callbacks->onContentRectChanged = lug::Main::onContentRectChanged;
 
-static void onResume(ANativeActivity *activity) {
-
-}
-
-static void *onSaveInstanceState(ANativeActivity *activity, size_t *outSize) {
-    return nullptr;
-}
-
-static void onPause(ANativeActivity *activity) {
-
-}
-
-static void onStop(ANativeActivity *activity) {
-
-}
-
-static void onDestroy(ANativeActivity *activity) {
-
-}
-
-static void onWindowFocusChanged(ANativeActivity *activity, int hasFocus) {
-
-}
-
-static void onNativeWindowCreated(ANativeActivity *activity, ANativeWindow *window) {
-
-}
-
-static void onNativeWindowResized(ANativeActivity *activity, ANativeWindow *window) {
-
-}
-
-static void onNativeWindowRedrawNeeded(ANativeActivity *activity, ANativeWindow *window) {
-
-}
-
-static void onNativeWindowDestroyed(ANativeActivity *activity, ANativeWindow *window) {
-
-}
-
-
-static void onInputQueueCreated(ANativeActivity *activity, AInputQueue *queue) {
-
-}
-
-static void onInputQueueDestroyed(ANativeActivity *activity, AInputQueue *queue) {
-
-}
-
-static void onContentRectChanged(ANativeActivity *activity, const ARect *rect) {
-
-}
-
-static void onConfigurationChanged(ANativeActivity *activity) {
-
-}
-
-static void onLowMemory(ANativeActivity *activity) {
-
-}
-
-void  ANativeActivity_onCreate(ANativeActivity *activity, void *savedState, size_t savedStateSize) {
-    activity->callbacks->onInputQueueCreated = onInputQueueCreated;
-    activity->callbacks->onInputQueueDestroyed = onInputQueueDestroyed;
-
-    activity->callbacks->onNativeWindowCreated = onNativeWindowCreated;
-    activity->callbacks->onNativeWindowDestroyed = onNativeWindowDestroyed;
-    activity->callbacks->onNativeWindowResized = onNativeWindowResized;
-    activity->callbacks->onNativeWindowRedrawNeeded = onNativeWindowRedrawNeeded;
-    activity->callbacks->onContentRectChanged = onContentRectChanged;
-
-    activity->callbacks->onWindowFocusChanged = onWindowFocusChanged;
-    activity->callbacks->onConfigurationChanged = onConfigurationChanged;
-    activity->callbacks->onLowMemory = onLowMemory;
-    activity->callbacks->onStart = onStart;
-    activity->callbacks->onResume = onResume;
-    activity->callbacks->onSaveInstanceState = onSaveInstanceState;
-    activity->callbacks->onPause = onPause;
-    activity->callbacks->onStop = onStop;
-    activity->callbacks->onDestroy = onDestroy;
+    activity->callbacks->onWindowFocusChanged = lug::Main::onWindowFocusChanged;
+    activity->callbacks->onConfigurationChanged = lug::Main::onConfigurationChanged;
+    activity->callbacks->onLowMemory = lug::Main::onLowMemory;
+    activity->callbacks->onStart = lug::Main::onStart;
+    activity->callbacks->onResume = lug::Main::onResume;
+    activity->callbacks->onSaveInstanceState = lug::Main::onSaveInstanceState;
+    activity->callbacks->onPause = lug::Main::onPause;
+    activity->callbacks->onStop = lug::Main::onStop;
+    activity->callbacks->onDestroy = lug::Main::onDestroy;
 
     activity->instance = lug::Main::lug_app_create(activity, savedState, savedStateSize);
 }
