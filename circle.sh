cf97f450c5a1d3ee73a7b52aaeb2530298a35eac
Corentin Chardeau <corentin.chardeau@epitech.eu>
2016-11-17 04:37:59 +0100

    Feature googletest with circle-ci & appveyor (#17)
    Feature googletest with circle-ci & appveyor

---
+++ circle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+case $CIRCLE_NODE_INDEX in
+  0)  export CXX=clang++
+      mkdir build && cd build
+      cmake ../ -DENABLE_TEST=true -DTEST_OUTPUT=$CIRCLE_TEST_REPORTS
+      make all test
+      ;;
+  1)  export CXX=g++
+      mkdir build && cd build
+      cmake ../ -DENABLE_TEST=true -DTEST_OUTPUT=$CIRCLE_TEST_REPORTS
+      make all test
+      ;;
+esac
+
--- samples/base/main.cpp
+++ samples/base/main.cpp
@@ -1,4 +1,13 @@
-int main(int, const char*[])
-{
+#include <lug/Config.hpp>
+#include <lug/System/Exception.hpp>
+#include <iostream>
+
+int main() {
+    try {
+        LUG_EXCEPT(IOException, "My exception worked");
+    }
+    catch (const lug::System::IOException &e) {
+        std::cout << e.getLine() << std::endl;
+    }
     return 0;
 }
