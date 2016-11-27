a3a03739468803e0b71c8ae8530f471f4f4fb2aa
Quentin01 <quentin.buathier@epitech.eu>
2016-11-28 03:04:35 +0900

    Add some constants

--- include/lug/Math/Constant.hpp
+++ include/lug/Math/Constant.hpp
@@ -10,6 +10,78 @@ namespace Math {
 template <typename T>
 T epsilon();
 
+template <typename T>
+T pi();
+
+template <typename T>
+T twoPi();
+
+template <typename T>
+T rootPi();
+
+template <typename T>
+T halfPi();
+
+template <typename T>
+T threeOverTwoPi();
+
+template <typename T>
+T quarterPi();
+
+template <typename T>
+T oneOverPi();
+
+template <typename T>
+T oneOverTwoPi();
+
+template <typename T>
+T twoOverPi();
+
+template <typename T>
+T fourOverPi();
+
+template <typename T>
+T twoOverRootPi();
+
+template <typename T>
+T oneOverRootPi();
+
+template <typename T>
+T rootHalfPi();
+
+template <typename T>
+T rootTwoPi();
+
+template <typename T>
+T rootLnFour();
+
+template <typename T>
+T e();
+
+template <typename T>
+T euler();
+
+template <typename T>
+T rootTwo();
+
+template <typename T>
+T rootThree();
+
+template <typename T>
+T rootFive();
+
+template <typename T>
+T lnTwo();
+
+template <typename T>
+T lnTen();
+
+template <typename T>
+T lnLnTwo();
+
+template <typename T>
+T goldenRatio();
+
 #include <lug/Math/Constant.inl>
 
 }
