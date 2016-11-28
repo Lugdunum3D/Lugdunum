3cf9da33588349eaee4268789e17ae05d164a4df
Yoann Long <yoann.long@epitech.eu>
2016-11-28 03:16:41 -0500

    small fix

--- samples/window/main.cpp
+++ samples/window/main.cpp
@@ -2,13 +2,10 @@
 #include <lug/Window/Window.hpp>
 #include <string>
 
-#include <android/log.h>
-
-#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "vulkanExample", __VA_ARGS__))
-#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "vulkanExample", __VA_ARGS__))
-#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, "vulkanExample", __VA_ARGS__))
-#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "vulkanExample", __VA_ARGS__))
-
+#if defined(__ANDROID__)
+    #include <android/log.h>
+    #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "lugdunum", __VA_ARGS__))
+#endif
 int main() {
     auto window = lug::Window::Window::create(800, 600, std::string("Default Window"), lug::Window::Style::Default);
     
@@ -20,7 +17,9 @@ int main() {
     while (window->isOpen()) {
         lug::Window::Event event;
         while (window->pollEvent(event)) {
-            LOGI("%d\n", event.type);
+            #if defined(__ANDROID__)
+                LOGI("%d\n", event.type);
+            #endif
             if (event.type == lug::Window::EventType::CLOSE) {
                 window->close();
             }
