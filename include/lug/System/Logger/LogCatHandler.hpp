2e0b6742e3eb557f4bdfb4ed15389baf0b399ed6
Antoine Bolvy <antoine.bolvy@gmail.com>
2016-12-17 12:47:16 -0500

    Android support

---
+++ include/lug/System/Logger/LogCatHandler.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <android/log.h>
+#include <lug/System/Logger/Handler.hpp>
+#include <lug/System/Exception.hpp>
+
+namespace lug {
+namespace System {
+
+class LogCatHandler : public Handler {
+public:
+    LogCatHandler(const std::string& name) : Handler(name) {
+    }
+    virtual ~LogCatHandler() {
+    }
+
+protected:
+    virtual void handle(const priv::Message& msg) {
+        __android_log_write(lugLevelToLogCatPrio(msg.level), msg.loggerName.c_str(), msg.formatted.c_str());
+    }
+    virtual void flush() {
+        // std::cout << std::flush;
+    }
+
+private:
+    android_LogPriority lugLevelToLogCatPrio(int level) {
+        switch (level) {
+            case lug::System::Level::Off:       return ANDROID_LOG_SILENT;
+            case lug::System::Level::Debug:     return ANDROID_LOG_DEBUG;
+            case lug::System::Level::Info:      return ANDROID_LOG_INFO;
+            case lug::System::Level::Warning:   return ANDROID_LOG_WARN;
+            case lug::System::Level::Error:     return ANDROID_LOG_ERROR;
+            case lug::System::Level::Fatal:     return ANDROID_LOG_FATAL;
+            case lug::System::Level::Assert:    return ANDROID_LOG_FATAL;
+        }
+        return ANDROID_LOG_UNKNOWN;
+    }
+};
+
+} // namespace lug
+} // namespace System
--- samples/window/main.cpp
+++ samples/window/main.cpp
@@ -1,20 +1,32 @@
 #include <lug/Window/Window.hpp>
+#include <lug/System/Logger.hpp>
+#include <lug/System/Logger/LogCatHandler.hpp>
 
 int main() {
     auto window = lug::Window::Window::create(800, 600, "Default Window", lug::Window::Style::Default);
 
+    auto logger = lug::System::makeLogger("LugdunumSamples");
+    auto handler = lug::System::makeHandler<lug::System::LogCatHandler>("LogCat");
+
+    logger->addHandler(handler);
+
     if (!window) {
+        logger->fatal("Window was not created");
         return 1;
     }
+
+    logger->info("Window was created successfully");
 
     // While window is open execute the following
     while (window->isOpen()) {
         lug::Window::Event event;
         while (window->pollEvent(event)) {
+            logger->info("Event received: {}", static_cast<uint32_t>(event.type));
+
             if (event.type == lug::Window::EventType::CLOSE) {
+                logger->info("Closing the app");
                 window->close();
             }
-
         }
 
         // ...
