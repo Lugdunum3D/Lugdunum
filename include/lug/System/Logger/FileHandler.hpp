9d4fa1ef0ba97e84205884fa00099e38966489c9
Antoine Bolvy <antoine.bolvy@gmail.com>
2016-12-18 20:18:47 -0500

    Tests for FileHandler

--- include/lug/System/Logger/FileHandler.hpp
+++ include/lug/System/Logger/FileHandler.hpp
@@ -1,32 +1,33 @@
 #pragma once
 
 #include <fstream>
-#include <lug/System/Logger/Handler.hpp>
 #include <lug/System/Exception.hpp>
+#include <lug/System/Logger/Handler.hpp>
+#include <lug/System/Logger/Message.hpp>
 
 namespace lug {
 namespace System {
 
-class FileHandler : public Handler {
+class LUG_SYSTEM_API FileHandler : public Handler {
 public:
     FileHandler(const std::string& name, const filename_t& filename, bool truncate) : Handler(name) {
-        _ofs.open(filename, std::ofstream::out);
+        std::ios_base::openmode mode = std::ofstream::out;
+        if (truncate)
+            mode |= std::ofstream::trunc;
+        _ofs.open(filename, mode);
         if (!_ofs.good()) {
             // TODO: add file to lug except when it handles variadic args
             LUG_EXCEPT(FileNotFoundException, "Failed to open file");
         }
-        // TODO: use this parameter
-        (void)truncate;
     }
-    virtual ~FileHandler() {
+    ~FileHandler() {
         _ofs.close();
     }
 
-protected:
-    virtual void handle(const priv::Message& msg) {
+    void handle(const priv::Message& msg) {
         _ofs << msg.formatted.c_str();
     }
-    virtual void flush() {
+    void flush() {
         _ofs.flush();
     }
 
