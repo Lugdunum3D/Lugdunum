d9e9b5dae2cfdfa4e397d537ff68e656abda6cb9
Nokitoo <guillaume.labey@epitech.eu>
2017-02-16 16:50:39 +0900

    Use vertices material diffuse as color

--- src/lug/Graphics/ObjModelLoader.cpp
+++ src/lug/Graphics/ObjModelLoader.cpp
@@ -13,64 +13,95 @@ bool ObjModelLoader::loadFromFile(Model* model, const std::string& file) {
     std::vector<tinyobj::material_t> materials;
     std::string err;
 
-    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, file.c_str())) {
+    int result = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, file.c_str());
+    if (err.size() != 0) {
         LUG_LOG.error("ObjModelLoader::loadFromFile error loading lodel \"{}\": {}", file, err);
+    }
+
+    if (!result) {
         return false;
     }
 
+
     uint32_t verticesOffset = 0;
-    uint32_t indicesOffset = 0;
+    uint32_t meshIndicesOffset = 0;
 
+    LUG_LOG.info("Loading model \"{}\"", file);
+    LUG_LOG.info("Nb Materials: {}", materials.size());
     for (const auto& shape : shapes) {
-        std::unique_ptr<Model::Mesh> mesh = std::make_unique<Model::Mesh>("mesh", verticesOffset, indicesOffset);
-        for (const auto& index : shape.mesh.indices) {
-            // TODO: Remove duplicates
-            Mesh::Vertex vertex;
-            vertex.pos = {
-                attrib.vertices[3 * index.vertex_index + 0],
-                attrib.vertices[3 * index.vertex_index + 1],
-                attrib.vertices[3 * index.vertex_index + 2]
-            };
-            vertex.normal = {
-                attrib.normals[3 * index.vertex_index + 0],
-                attrib.normals[3 * index.vertex_index + 1],
-                attrib.normals[3 * index.vertex_index + 2]
-            };
+        uint32_t indicesOffset = 0;
+        uint32_t faceNb = 0;
+        std::unique_ptr<Model::Mesh> mesh = std::make_unique<Model::Mesh>("mesh", verticesOffset, meshIndicesOffset);
 
-            if (attrib.texcoords.size() > 0) {
-                vertex.uv = {
-                    attrib.texcoords[2 * index.vertex_index + 0],
-                    1.0f - attrib.texcoords[2 * index.vertex_index + 1]
+        // Loop over faces
+        for (const auto& faceVertices: shape.mesh.num_face_vertices) {
+            // Loop over face vertices
+            for (uint32_t i = 0; i < faceVertices; ++i) {
+                tinyobj::index_t index = shape.mesh.indices[indicesOffset + i];
+                tinyobj::material_t* material = nullptr;
+
+                if (shape.mesh.material_ids.size() > 0 && shape.mesh.material_ids[faceNb] != -1) {
+                    material = &materials[shape.mesh.material_ids[faceNb]];
+                }
+
+                Mesh::Vertex vertex;
+                vertex.pos = {
+                    attrib.vertices[3 * index.vertex_index + 0],
+                    attrib.vertices[3 * index.vertex_index + 1],
+                    attrib.vertices[3 * index.vertex_index + 2]
                 };
-            }
-            vertex.color = {
-                0.0f, 0.0f, 1.0f
-            };
+                vertex.normal = {
+                    attrib.normals[3 * index.vertex_index + 0],
+                    attrib.normals[3 * index.vertex_index + 1],
+                    attrib.normals[3 * index.vertex_index + 2]
+                };
 
-            uint32_t vertexIndice = 0;
-            // Search duplicate vertex
-            {
-                bool duplicate = false;
-                for (auto& meshVertex: mesh->vertices) {
-                    if (meshVertex == vertex) {
-                        duplicate = true;
-                        break;
+                if (attrib.texcoords.size() > 0) {
+                    vertex.uv = {
+                        attrib.texcoords[2 * index.vertex_index + 0],
+                        1.0f - attrib.texcoords[2 * index.vertex_index + 1]
+                    };
+                }
+
+                if (material) {
+                    vertex.color = {
+                        material->diffuse[0],
+                        material->diffuse[1],
+                        material->diffuse[2]
+                    };
+                }
+                else {
+                    vertex.color = {
+                        1.0f, 1.0f, 1.0f
+                    };
+                }
+
+                uint32_t vertexIndice = 0;
+                // Search duplicate vertex
+                {
+                    bool duplicate = false;
+                    for (auto& meshVertex: mesh->vertices) {
+                        if (meshVertex == vertex) {
+                            duplicate = true;
+                            break;
+                        }
+                        vertexIndice++;
                     }
-                    vertexIndice++;
+
+                    // Add vertex
+                    if (!duplicate) {
+                        mesh->vertices.push_back(vertex);
+                        verticesOffset++;
+                    }
                 }
 
-                // Add vertex
-                if (!duplicate) {
-                    mesh->vertices.push_back(vertex);
-                    verticesOffset++;
-                }
+                mesh->indices.push_back(vertexIndice);
             }
-
-            mesh->indices.push_back(vertexIndice);
-            indicesOffset++;
-
+            indicesOffset += faceVertices;
+            faceNb++;
         }
         model->addMesh(std::move(mesh));
+        meshIndicesOffset += indicesOffset;
     }
 
     return true;
