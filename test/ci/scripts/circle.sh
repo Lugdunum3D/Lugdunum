eb02b39c3253d76dca7394420d7f188a3afad58c
Quentin01 <quentin.buathier@epitech.eu>
2017-02-28 16:46:17 +0900

    Install Lugdunum

--- test/ci/scripts/circle.sh
+++ test/ci/scripts/circle.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 case $CIRCLE_NODE_INDEX in
-  0)  export CXX=clang++
-      mkdir build && cd build
-      cmake ../ -DBUILD_TESTS=true -DTEST_OUTPUT=$CIRCLE_TEST_REPORTS
-      make all test
-      ;;
-  1)  export CXX=g++
-      mkdir build && cd build
-      cmake ../ -DBUILD_TESTS=true -DTEST_OUTPUT=$CIRCLE_TEST_REPORTS
-      make all test
-      ;;
+    0)
+        export CXX=clang++
+        mkdir install
+        mkdir build && cd build
+        cmake ../ -DBUILD_TESTS=true -DTEST_OUTPUT=$CIRCLE_TEST_REPORTS -DCMAKE_INSTALL_PREFIX=../install
+        make all install test
+    ;;
+
+    1)
+        export CXX=g++
+        mkdir build && cd build
+        cmake ../ -DBUILD_TESTS=true -DTEST_OUTPUT=$CIRCLE_TEST_REPORTS -DCMAKE_INSTALL_PREFIX=../install
+        make all install test
+    ;;
 esac
 
