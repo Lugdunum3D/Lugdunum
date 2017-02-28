5816814676d4c81495cab1972baed688f61546cc
Quentin01 <quentin.buathier@epitech.eu>
2017-02-28 16:50:58 +0900

    Build samples on CircleCI

--- test/ci/scripts/circle.sh
+++ test/ci/scripts/circle.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+function build_samples() {
+    cd ~/Lugdunum
+
+    for sample in `find samples -mindepth 1 -maxdepth 1 ! -name "compiler" -type d`
+    do
+        echo "Building $sample"
+        cd ~/Lugdunum/$sample
+        mkdir build && cd build
+        (cmake .. && make all) || return 1
+    done
+
+    return 0
+}
+
 case $CIRCLE_NODE_INDEX in
     0)
         export CXX=clang++
-        mkdir install
         mkdir build && cd build
-        cmake ../ -DBUILD_TESTS=true -DTEST_OUTPUT=$CIRCLE_TEST_REPORTS -DCMAKE_INSTALL_PREFIX=../install
-        make all install test
+        cmake .. -DBUILD_TESTS=true -DTEST_OUTPUT=$CIRCLE_TEST_REPORTS
+        make all test && sudo make install && build_samples
     ;;
 
     1)
         export CXX=g++
         mkdir build && cd build
-        cmake ../ -DBUILD_TESTS=true -DTEST_OUTPUT=$CIRCLE_TEST_REPORTS -DCMAKE_INSTALL_PREFIX=../install
-        make all install test
+        cmake .. -DBUILD_TESTS=true -DTEST_OUTPUT=$CIRCLE_TEST_REPORTS
+        make all test && sudo make install && build_samples
     ;;
 esac
 
