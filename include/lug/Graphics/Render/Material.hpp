462073ab6c02e954d222c576a6f97a2c9f49fe5c
Quentin01 <quentin.buathier@epitech.eu>
2017-05-09 22:18:27 +0900

    Put all the constants of a Material in the same structure

--- include/lug/Graphics/Render/Material.hpp
+++ include/lug/Graphics/Render/Material.hpp
@@ -28,19 +28,13 @@ public:
         uint32_t texCoord{0};
     };
 
-    struct Factors {
-        Math::Vec4f baseColor{1.0f};    ///< Overall color of the material.
-        Math::Vec3f emissive{0.0f};     ///< Color of the emissive factor.
-        float metallic{0.0f};           ///< How metal-like the material is. 0 is non-metal (e.g. wood, plastic), 1 is metallic.
-        float roughness{0.0f};          ///< How rough the material is. 0 is smooth and 1 is rough (matte).
-    };
-
-    struct NormalTextureInfo : public TextureInfo {
-        float scale{0.0f};
-    };
-
-    struct OcclusionTextureInfo : public TextureInfo {
-        float strength{0.0f};
+    struct Constants {
+        Math::Vec4f baseColor{1.0f};            ///< Overall color of the material.
+        Math::Vec3f emissive{0.0f};             ///< Color of the emissive factor.
+        float metallic{0.0f};                   ///< How metal-like the material is. 0 is non-metal (e.g. wood, plastic), 1 is metallic.
+        float roughness{0.0f};                  ///< How rough the material is. 0 is smooth and 1 is rough (matte).
+        float normalTextureScale{1.0f};         ///< Scale of the normal texture
+        float occlusionTextureStrength{1.0f};   ///< Strength of the occlusion texture
     };
 
 public:
@@ -66,11 +60,11 @@ public:
 protected:
     std::string _name;
 
-    Factors _factors;
+    Constants _constants;
     TextureInfo _baseColorTexture;
     TextureInfo _metallicRoughnessTexture;
-    NormalTextureInfo _normalTexture;
-    OcclusionTextureInfo _occlusionTexture;
+    TextureInfo _normalTexture;
+    TextureInfo _occlusionTexture;
     TextureInfo _emissiveTexture;
 };
 
--- src/lug/Graphics/Builder/Material.cpp
+++ src/lug/Graphics/Builder/Material.cpp
@@ -11,19 +11,19 @@ void Material::Material::setName(const std::string& name) {
 }
 
 void Material::setBaseColorFactor(const Math::Vec4f& factor) {
-    _factors.baseColor = factor;
+    _constants.baseColor = factor;
 }
 
 void Material::setEmissiveFactor(const Math::Vec3f& factor) {
-    _factors.emissive = factor;
+    _constants.emissive = factor;
 }
 
 void Material::setMetallicFactor(float factor) {
-    _factors.metallic = factor;
+    _constants.metallic = factor;
 }
 
 void Material::setRoughnessFactor(float factor) {
-    _factors.roughness = factor;
+    _constants.roughness = factor;
 }
 
 void Material::setBaseColorTexture(Resource::SharedPtr<Resource> texture, uint32_t texCoord) {
@@ -39,13 +39,13 @@ void Material::setMetallicRoughnessTexture(Resource::SharedPtr<Resource> texture
 void Material::setNormalTexture(Resource::SharedPtr<Resource> texture, uint32_t texCoord, float scale) {
     _normalTexture.texture = texture;
     _normalTexture.texCoord = texCoord;
-    _normalTexture.scale = scale;
+    _constants.normalTextureScale = scale;
 }
 
 void Material::setOcclusionTexture(Resource::SharedPtr<Resource> texture, uint32_t texCoord, float strength) {
     _occlusionTexture.texture = texture;
     _occlusionTexture.texCoord = texCoord;
-    _occlusionTexture.strength = strength;
+    _constants.occlusionTextureStrength = strength;
 }
 
 void Material::setEmissiveTexture(Resource::SharedPtr<Resource> texture, uint32_t texCoord) {
