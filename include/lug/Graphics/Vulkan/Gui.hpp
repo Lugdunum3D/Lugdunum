41f873c689ac977a4ca6a21c505b0523fc8c5a48
StuartSulaski <stuartsulaski@gmail.com>
2017-06-02 01:21:16 +0900

    Updated CMakeList to unclude imgui header and source

---
+++ include/lug/Graphics/Vulkan/Gui.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <lug/Graphics/Export.hpp>
+
+namespace lug {
+namespace Graphics {
+namespace Vulkan {
+
+class LUG_GRAPHICS_API Gui {
+};
+
+} // Vulkan
+} // Graphics
+} // lug
